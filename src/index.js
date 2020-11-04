7a48eee7b5cec426900dc7ff5cddec9ef23d7c94
Ana Beatriz Costa de Oliveira <anabeatrizcosta.o@gmail.com>
2020-11-04 14:57:04 -0300

    Manipulação DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const buttonCodificar = document.getElementById("codificar");
+buttonCodificar.addEventListener("click", codificar);
+function codificar() {
+    const maiuscula = document.getElementById("textCode");
+    maiuscula.value = maiuscula.value.toUpperCase();
+    const mensagem = document.getElementById("textCode").value;
+    const offset = Number(document.getElementById("offsetCode").value);
+    const mensagemCodificada = cipher.encode(offset, mensagem);
+    document.getElementById("fraseCodificada").innerHTML = `${mensagemCodificada}`;
+}
+
+const buttonDecodificar = document.getElementById("decodificar");
+buttonDecodificar.addEventListener("click", decodificar);
+function decodificar() {
+    const maiuscula = document.getElementById("textDecode");
+    maiuscula.value = maiuscula.value.toUpperCase();
+    const mensagemDec = document.getElementById("textDecode").value;
+    const offsetDec = Number(document.getElementById("offsetDecode").value);
+    const mensagemDecodificada = cipher.decode(offsetDec, mensagemDec);
+    document.getElementById("fraseDecodificada").innerHTML = `${mensagemDecodificada}`;
+}

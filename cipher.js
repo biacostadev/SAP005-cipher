7d63310b4cf9c49b9a6ac28da9015a5bc542013c
Ana Beatriz Costa de Oliveira <anabeatrizcosta.o@gmail.com>
2020-11-06 17:50:10 -0300

    Modificações de estrutura

--- cipher.js
+++ cipher.js
@@ -1,34 +1,62 @@
+const alfabetoMaiusculo = 65;
+const alfabetoMinusculo = 97;
+const tamanhoAlfabeto = 26;
+
 const cipher = {
   encode(offset, mensagem) {
+    let letrasCifradas;
     let palavraCifrada = "";
-    if (offset == null || !mensagem){
-      throw new TypeError("Erro!");
-      }else{
-        for (let i = 0; i < mensagem.length; i++) {
-          const cifrar = mensagem.charCodeAt(i);
-          let codificado = ((cifrar - 65 + offset) % 26) + 65;
-          if (cifrar <= 64 || cifrar >= 91) {
-            codificado = cifrar;
-          } palavraCifrada += String.fromCharCode(codificado);
-        }
-      return palavraCifrada;
-    }
-  },
 
-  decode(offsetDec, mensagemDec) {
-    let palavraDecifrada = "";
-    if (offsetDec == null || !mensagemDec) {
-      throw new TypeError("Erro!");
-      } else {
-        for (let i = 0; i < mensagemDec.length; i++) {
-          const decifrar = mensagemDec.charCodeAt(i);
-          let decodificado = ((decifrar - 65 -(offsetDec % 26) + 26) % 26) + 65;
-          if (decifrar <= 64 || decifrar >= 91) {
-            decodificado = decifrar;
-          } palavraDecifrada += String.fromCharCode(decodificado);
+    if (offset == null || !mensagem) throw new TypeError("Por favor, verifique sua mensagem ou sua chave de criptografia!");
+
+    for (let i = 0; i < mensagem.length; i++) {
+      const cifrar = mensagem.charCodeAt(i);
+      if (cifrar >= 65 && cifrar <= 90) {
+        if (offset > 0) {
+          letrasCifradas = ((cifrar - alfabetoMaiusculo) + offset) % tamanhoAlfabeto + alfabetoMaiusculo;
+        } else {
+          letrasCifradas = ((cifrar - alfabetoMaiusculo) + (tamanhoAlfabeto + offset % tamanhoAlfabeto)) % tamanhoAlfabeto + alfabetoMaiusculo;
         }
-        return palavraDecifrada;
+      } else if (cifrar >= 97 && cifrar <= 122) {
+        if (offset > 0) {
+          letrasCifradas = ((cifrar - alfabetoMinusculo) + offset) % tamanhoAlfabeto + alfabetoMinusculo;
+        } else {
+          letrasCifradas = ((cifrar - alfabetoMinusculo) + (tamanhoAlfabeto + offset % tamanhoAlfabeto)) % tamanhoAlfabeto + alfabetoMinusculo;
+        }
+      } else {
+        letrasCifradas = cifrar;
+      }
+      palavraCifrada += String.fromCharCode(letrasCifradas);
     }
+    return palavraCifrada;
+  },
+  decode(offsetDec, mensagemDec) {
+    let letrasDecifradas;
+    let palavraDecifrada = "";
+    let offsetNegativo = offsetDec % tamanhoAlfabeto;
+
+    if (offsetDec == null || !mensagemDec) throw new TypeError("Por favor, verifique sua mensagem ou sua chave de criptografia!");
+
+    for (let i = 0; i < mensagemDec.length; i++) {
+      let decifrar = mensagemDec.charCodeAt(i);
+      if (decifrar >= 65 && decifrar <= 90) {
+        if (offsetNegativo > 0) {
+          letrasDecifradas = ((decifrar + alfabetoMaiusculo) - offsetNegativo) % tamanhoAlfabeto + alfabetoMaiusculo;
+        } else {
+          letrasDecifradas = ((decifrar + alfabetoMaiusculo) - (tamanhoAlfabeto + offsetNegativo % tamanhoAlfabeto)) % tamanhoAlfabeto + alfabetoMaiusculo;
+        }
+      } else if (decifrar >= 97 && decifrar <= 122) {
+        if (offsetNegativo > 0) {
+          letrasDecifradas = ((decifrar + alfabetoMinusculo) - offsetNegativo - 12) % tamanhoAlfabeto + alfabetoMinusculo;
+        } else {
+          letrasDecifradas = ((decifrar + alfabetoMinusculo) - (tamanhoAlfabeto + offsetNegativo % tamanhoAlfabeto) - 12) % tamanhoAlfabeto + alfabetoMinusculo;
+        }
+      } else {
+        letrasDecifradas = decifrar;
+      }
+      palavraDecifrada += String.fromCharCode(letrasDecifradas)
+    }
+    return palavraDecifrada;
   }
 };
 
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import cipher from './cipher.js';
 const buttonCodificar = document.getElementById("codificar");
 buttonCodificar.addEventListener("click", codificar);
 function codificar() {
-    const maiuscula = document.getElementById("textCode");
-    maiuscula.value = maiuscula.value.toUpperCase();
     const mensagem = document.getElementById("textCode").value;
     const offset = Number(document.getElementById("offsetCode").value);
     const mensagemCodificada = cipher.encode(offset, mensagem);
@@ -14,8 +12,6 @@ function codificar() {
 const buttonDecodificar = document.getElementById("decodificar");
 buttonDecodificar.addEventListener("click", decodificar);
 function decodificar() {
-    const maiuscula = document.getElementById("textDecode");
-    maiuscula.value = maiuscula.value.toUpperCase();
     const mensagemDec = document.getElementById("textDecode").value;
     const offsetDec = Number(document.getElementById("offsetDecode").value);
     const mensagemDecodificada = cipher.decode(offsetDec, mensagemDec);
